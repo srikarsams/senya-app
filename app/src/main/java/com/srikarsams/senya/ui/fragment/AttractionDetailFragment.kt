726f56c85faf84e75b50642e4dea9d3091b73e44
srikarsam <srikar_samudrala@condenast.com>
2022-01-10 21:10:48 +0530

    Implemented Detail fragment

--- app/src/main/java/com/srikarsams/senya/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/srikarsams/senya/ui/fragment/AttractionDetailFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
 import com.srikarsams.senya.databinding.FragmentAttractionDetailBinding
 
-class AttractionDetailFragment: BaseFragment() {
+class AttractionDetailFragment : BaseFragment() {
     private var _binding: FragmentAttractionDetailBinding? = null
     private val binding get() = _binding!!
 
     private val safeArgs: AttractionDetailFragmentArgs by navArgs()
+    private val attraction by lazy {
+        attractions.find { it.id == safeArgs.attractionId }!!
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +29,15 @@ class AttractionDetailFragment: BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.button.text = safeArgs.attractionId
+        binding.titleTextView.text = attraction.title
+        Picasso.get().load(attraction.image_urls[0]).into(binding.headerImageView)
+        binding.descriptionTextView.text =
+            attraction.description
+        binding.monthsToVisitTextView.text = attraction.months_to_visit
+        binding.numberOfFactsTextView.text = "${attraction.facts.size.toString()} Facts"
+        binding.numberOfFactsTextView.setOnClickListener {
+
+        }
     }
 
     override fun onDestroyView() {
